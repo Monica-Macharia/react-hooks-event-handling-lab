992bf422949bb1ed96bb7de775e11ad2a826ee20
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-07-16 15:36:01 +0300

    event handlers

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+    function focusFun(){
+        console.log('Good!');
+    }
+    function blurFun(){
+        console.log('Hey! Eyes on me!');
+    }
+
+    return(
+        <button onFocus= {focusFun} onBlur ={blurFun}>Eyes on me</button>
+        );
+}
+
+
+
+
+export default EyesOnMe;
